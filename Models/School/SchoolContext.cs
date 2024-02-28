6f8c06e6ffb7c2ac065c61195a5259a3a61f89a5
redsaeok <74396365+redsaeok@users.noreply.github.com>
2024-02-27 20:50:08 -0800

    first commit

---
+++ Models/School/SchoolContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureFunctions4602.Models.School;
+
+public partial class SchoolContext : DbContext
+{
+    public SchoolContext()
+    {
+    }
+
+    public SchoolContext(DbContextOptions<SchoolContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=localhost,1443;Database=School;User ID=sa;Password=SqlPassword!;TrustServerCertificate=True;");
+
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
